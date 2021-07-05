0e6b53884c9052729811940787d33577f528d273
devops3321 <77125396+devops3321@users.noreply.github.com>
2021-07-05 13:50:09 +0530

    Delete Terraform_files directory

--- Terraform_files/script.sh
+++
@@ -1,4 +0,0 @@
-apt-get update -y 
-apt-get install unzip curl wget
-apt-get install java -y 
-apt-get install ngnix -y 
--- Terraform_files/values.tfvars
+++
@@ -1,3 +0,0 @@
-access_key  = "AKIAWKR3A4F6IMMVBPPO"
-secret_key = "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"
-
