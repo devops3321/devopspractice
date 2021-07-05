12eddd53f3e80c245601874ad10c8b33f56c9534
devops3321 <77125396+devops3321@users.noreply.github.com>
2021-07-05 13:49:26 +0530

    Delete FUNCTIONS directory

--- FUNCTIONS/allarg_type.py
+++
@@ -1,7 +0,0 @@
-# add country='USA'
-def allDataParam(name,*lang,country='USA',**books):
-    print(name,lang,books,country) # John ('English', 'Spanish', 'Deuth') {'book1': 'author', 'book2': 'author2'}
-    
-
-allDataParam('John','English','Spanish','Deutsch','Hindi',country='INDIA', book1='author1', book2 = 'author2', book3 = 'author3')
-
--- FUNCTIONS/empty.py
+++
@@ -1,2 +0,0 @@
-# no code
-print(dir())
